1211ece68f1c512fee64c51936b508fb0c261bdf
Aquib Baig <aquibbaig97@gmail.com>
2020-01-12 19:04:41 +0530

    Feat: Connect  to Postgres. URI is available on Heroku, Refer to <aquibbaig97@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"net/http"
+	"log"
+	"os"
+	"fmt"
 
+	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/contrib/static"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"database/sql"
 )
 
 func  main()  {
 	//set the router as the default shipped with gin
 	router := gin.Default()
+
+	// connect to postgres
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	uri := os.Getenv("DB_URI")
+	db, err := sql.Open("postgres", uri)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Ping()
+	fmt.Println("Connected")
+
 
 	//serve frontend static files
 	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
@@ -23,7 +44,7 @@ func  main()  {
 			})
 		})
 	}
-
+	
 	//start and run the server
 	router.Run(":5000")
 }
